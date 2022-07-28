1e5c60970fdb08d41ec0379ec804ffb2d8109a67
Jordan anderson <mr@jordananderson.io>
2022-07-28 02:21:13 +0100

    Print out environment variables

--- build/Program.cs
+++ build/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Build.Context;
 using Cake.Frosting;
@@ -9,7 +10,13 @@ public static class Program
 {
   public static int Main(string[] args)
   {
-    Console.WriteLine(string.Join(" ; ", args));
+    Console.WriteLine("Arguments");
+    Console.WriteLine("-----------------------");
+    Console.WriteLine(string.Join(Environment.NewLine, args));
+    Console.WriteLine("Environment");
+    Console.WriteLine("-----------------------");
+    var variables = Environment.GetEnvironmentVariables();
+    Console.WriteLine(string.Join(Environment.NewLine, variables.Keys.Cast<string>().Select(x => $"{x}={variables[x]}")));
 
     var isDeployment = args.Any(x => x.Contains("--target=Create Namespace")
                                      || x.Contains("--target=Deploy Helm Chart")
