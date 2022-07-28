502459a6d43a91ff49b0e8187e99ed3fdb3f0ea9
Jordan anderson <mr@jordananderson.io>
2022-07-28 03:44:13 +0100

    Remove output

--- build/Program.cs
+++ build/Program.cs
@@ -10,14 +10,6 @@ public static class Program
 {
   public static int Main(string[] args)
   {
-    Console.WriteLine("Arguments");
-    Console.WriteLine("-----------------------");
-    Console.WriteLine(string.Join(Environment.NewLine, args));
-    Console.WriteLine("Environment");
-    Console.WriteLine("-----------------------");
-    var variables = Environment.GetEnvironmentVariables();
-    Console.WriteLine(string.Join(Environment.NewLine, variables.Keys.Cast<string>().Select(x => $"{x}={variables[x]}")));
-
     var isDeployment = args.Any(x => x.Contains("--target=Create Namespace")
                                      || x.Contains("--target=Deploy Helm Chart")
                                      || x.Contains("--target=Transform Variables"));
